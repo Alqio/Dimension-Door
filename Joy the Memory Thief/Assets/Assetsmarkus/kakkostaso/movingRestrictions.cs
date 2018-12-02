f179f3b611e95ab828e703f90b1f4c9fc9ea0cd3
markus_laattala <markus.laattala@aalto.fi>
2018-12-02 16:42:04 +0200

    kakkos tasoa ja menu graffoja ja potilas graffa

--- Joy the Memory Thief/Assets/Assetsmarkus/kakkostaso/movingRestrictions.cs
+++ Joy the Memory Thief/Assets/Assetsmarkus/kakkostaso/movingRestrictions.cs
@@ -5,11 +5,13 @@ using UnityEngine;
 public class movingRestrictions : MonoBehaviour
 {
     // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public GameObject g;
 
+    private void OnTriggerStay2D(Collider2D collision)
+    {
+
+    }
+  
     // Update is called once per frame
     void Update()
     {
--- Joy the Memory Thief/Assets/Assetsmarkus/kakkostaso/soundPlatform.cs
+++ Joy the Memory Thief/Assets/Assetsmarkus/kakkostaso/soundPlatform.cs
@@ -41,12 +41,19 @@ public class soundPlatform : MonoBehaviour
     private void OnCollisionEnter2D(Collision2D collision)
     {
         if (collision.gameObject.tag == "Player")
+        {
             playerInRange = true;
+            collision.collider.transform.SetParent(transform);
+        }
+
     }
     private void OnCollisionExit2D(Collision2D collision)
     {
         if (collision.gameObject.tag == "Player")
+        {
+            collision.collider.transform.SetParent(null);
             playerInRange = false;
+        }
     }
     private void OnTriggerExit2D(Collider2D collision)
     {

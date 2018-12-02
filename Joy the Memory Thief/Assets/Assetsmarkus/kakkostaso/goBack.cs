82a4e6a73572ab4728026a3f233f10e9857947eb
markus_laattala <markus.laattala@aalto.fi>
2018-12-02 18:16:13 +0200

    alus palaa jos karkaa

---
+++ Joy the Memory Thief/Assets/Assetsmarkus/kakkostaso/goBack.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class goBack : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public Transform home;
+    public Transform player;
+    public Transform[] others;
+    public mic mic;
+    public float speed;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(this.transform.position.y > player.position.y  && Mathf.Abs(this.transform.position.y - home.position.y) > 0.5f && !mic.singing)
+        {
+            transform.position = transform.position + (home.position - transform.position).normalized * speed * Time.deltaTime;
+            foreach (Transform i in others)
+                i.position = transform.position;
+        }
+    }
+}
